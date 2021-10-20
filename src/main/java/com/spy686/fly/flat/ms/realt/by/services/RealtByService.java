d3d28ea0570f436cb8b5a361cfbb8ea8c8ff0c79
Pavel Sakharchuk <sakharchuk.pavel@gmail.com>
2021-10-20 23:32:11 +0300

    Updated RealtByService

--- src/main/java/com/spy686/fly/flat/ms/realt/by/services/RealtByService.java
+++ src/main/java/com/spy686/fly/flat/ms/realt/by/services/RealtByService.java
@@ -25,17 +25,10 @@ public class RealtByService {
 
     public void fetchRealtBy() {
         List<RentFlat> actualRentFlatList = rentFlatRestService.getRentFlatList();
-        Map<Long, RentFlat> actualRentFlatMap = actualRentFlatList.stream()
-                .collect(Collectors.toMap(RentFlat::getObjectId, Function.identity()));
-
         List<RentFlat> savedRentFlatList = rentFlatDatabaseService.getAll();
         Map<Long, RentFlat> savedRentFlatMap = savedRentFlatList.stream()
                 .distinct()
                 .collect(Collectors.toMap(RentFlat::getObjectId, Function.identity()));
-
-        List<RentFlat> existedRentFlatList = savedRentFlatList.stream()
-                .filter(savedRentFlat -> Optional.ofNullable(actualRentFlatMap.get(savedRentFlat.getObjectId())).isPresent())
-                .collect(Collectors.toList());
 
         // Update actualRentFlatList:
         // actual.PriceUsd = saved.PriceUsd: actual.CreateDate=saved.CreateDate; actual.LastUpdate=saved.LastUpdate
@@ -46,20 +39,21 @@ public class RealtByService {
                 .forEach(actualRentFlat -> {
                     RentFlat savedRentFlat = Optional.ofNullable(savedRentFlatMap.get(actualRentFlat.getObjectId())).orElse(null);
                     if (savedRentFlat != null) {
+                        actualRentFlat.setId(savedRentFlat.getId());
+                        actualRentFlat.setCreateDate(savedRentFlat.getCreateDate());
+
                         int actualPriceUsd = actualRentFlat.getPriceUsd() == null ? 0 : actualRentFlat.getPriceUsd();
                         int savedPriceUsd = savedRentFlat.getPriceUsd() == null ? 0 : savedRentFlat.getPriceUsd();
+
                         if (actualPriceUsd == savedPriceUsd) {
-                            actualRentFlat.setCreateDate(savedRentFlat.getCreateDate());
                             actualRentFlat.setLastUpdate(savedRentFlat.getLastUpdate());
                         }
                         if (actualPriceUsd != savedPriceUsd) {
-                            actualRentFlat.setCreateDate(savedRentFlat.getCreateDate());
                             actualRentFlat.setLastUpdate(LocalDateTime.now());
                         }
                     }
                 });
 
-        rentFlatDatabaseService.deleteAll(existedRentFlatList);
         rentFlatDatabaseService.saveAll(actualRentFlatList);
     }
 }
